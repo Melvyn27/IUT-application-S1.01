ac872a9f632a61ce547da41fa4885dba23eeb41e
Vanessa <jvanessa6023@gmail.com>
2021-12-08 09:51:11 +0100

    Auto stash before merge of "add_client" and "origin/add_client"

--- main.c
+++ main.c
@@ -40,7 +40,12 @@ int main()
   printf("%d ", function());
   printf("%d \n", function());
 
+<<<<<<< Updated upstream
    char c='\0'; // or c=0; //code ASCII
 
+=======
+   //char c='\0'; // or c=0; //code ASCII
+    
+>>>>>>> Stashed changes
     return 0;
 }
