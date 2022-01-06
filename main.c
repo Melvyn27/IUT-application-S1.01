d95f0ed929201373b355b150bfb5afadbf0b8f66
Melvyn27 <melvyn2701@gmail.com>
2022-01-06 08:51:41 +0100

    fix: remplir_client

--- main.c
+++ main.c
@@ -39,37 +39,37 @@ printf("%s\n", client[i].profession);
 }
 
 
-void remplirClient(char prenom[20], char nom[20], char adresse[20], char numTel[20], char adresseMail[20], char profession[20], struct abonne client){
+void remplirClient(char prenom[20], char nom[20], char adresse[20], char numTel[20], char adresseMail[20], char profession[20], struct abonne client[], int n){
     int mot = 0;
     int i;
     mot = 0;
         while(prenom[mot]!='\0'){
-        client.prenom[mot] = prenom[mot];
+        client[n].prenom[mot] = prenom[mot];
         mot = mot+1;}
         mot=0;
 
         while(nom[mot]!='\0'){
-        client.nom[mot] = nom[mot];
+        client[n].nom[mot] = nom[mot];
         mot = mot+1;}
         mot=0;
 
         while(adresse[mot]!='\0'){
-        client.adresse[mot] = adresse[mot];
+        client[n].adresse[mot] = adresse[mot];
         mot = mot+1;}
         mot=0;
 
         while(numTel[mot]!='\0'){
-        client.numTel[mot] = numTel[mot];
+        client[n].numTel[mot] = numTel[mot];
         mot = mot+1;}
         mot=0;
 
         while(adresseMail[mot]!='\0'){
-        client.adresseMail[mot] = adresseMail[mot];
+        client[n].adresseMail[mot] = adresseMail[mot];
         mot = mot+1;}
         mot=0;
 
         while(profession[mot]!='\0'){
-        client.profession[mot] = profession[mot];
+        client[n].profession[mot] = profession[mot];
         mot = mot+1;}
         mot=0;
     }
@@ -83,7 +83,7 @@ client[0].prenom[2] = 'l';
 client[0].prenom[3] = 'v';
 client[0].prenom[4] = 'y';
 client[0].prenom[5] = 'n';
-remplirClient("Melvyn","Delpree","Villes","0000000000","melvyn2701@gmail.com","etudiant", client[1]);
+remplirClient("Melvyn","Delpree","Villes","0000000000","melvyn2701@gmail.com","etudiant", client, 1);
 affiche(client);
 return 0;
 }
