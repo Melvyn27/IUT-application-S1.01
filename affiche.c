f92cac670e9fe7417d1b1b6efd2b3c4f2f7e7c89
Melvyn27 <melvyn2701@gmail.com>
2022-01-09 18:30:06 +0100

    fix: implementation

--- affiche.c
+++ affiche.c
@@ -5,30 +5,30 @@
 
 
 void triListe (struct abonne client[]){
-printf("\nliste trié");
+    printf("\nliste trié");
 }
 
 
 void affiche_all(struct abonne client[], int tri){
-if (tri == 1) triListe(client);
-int i;
-printf("\n\n");
-for (i=1; i<size; i++){
-printf("%s, ", client[i].prenom);
-printf("%s, ", client[i].nom);
-printf("%s, ", client[i].adresse);
-printf("%s, ", client[i].numTel);
-printf("%s, ", client[i].adresseMail);
-printf("%s\n", client[i].profession);
-}
+    if (tri == 1) triListe(client);
+    int i;
+    printf("\n\n");
+    for (i=1; i<size; i++){
+        printf("%s, ", client[i].prenom);
+        printf("%s, ", client[i].nom);
+        printf("%s, ", client[i].adresse);
+        printf("%s, ", client[i].numTel);
+        printf("%s, ", client[i].adresseMail);
+        printf("%s\n", client[i].profession);
+    }
 }
 
 
 void affiche_client(struct abonne client[], int n){
-printf("\n%s, ", client[n].prenom);
-printf("%s, ", client[n].nom);
-printf("%s, ", client[n].adresse);
-printf("%s, ", client[n].numTel);
-printf("%s, ", client[n].adresseMail);
-printf("%s\n", client[n].profession);
+    printf("\n%s, ", client[n].prenom);
+    printf("%s, ", client[n].nom);
+    printf("%s, ", client[n].adresse);
+    printf("%s, ", client[n].numTel);
+    printf("%s, ", client[n].adresseMail);
+    printf("%s\n", client[n].profession);
 }
--- main.c
+++ main.c
@@ -17,17 +17,16 @@ int openFile(char chemin[])
     return 1;
 }*/
 
-int main()
-{
-struct abonne client[size];
-remplirClient("none","none","none","none","none","none", client, 0);
-remplirClient("Melvyn","Delpree","Villes","0000000000","melvyn2701@gmail.com","etudiant", client, 1);
-affiche_all(client, 0);
-remplirClient("Liam", "Luka", "Lyon", "0000000000", "liamlucas@wanadoo.fr", "éleveur de pommes de terre", client, 2);
-affiche_all(client, 1);
-modifieClient("Lucas", 1, client, 2);
-affiche_all(client, 1);
-printf("%d", recherche(client, "Melvyn"));
-affiche_all(client, 0);
-return 0;
+int main(){
+    struct abonne client[size];
+    remplirClient("none","none","none","none","none","none", client, 0);
+    remplirClient("Melvyn","Delpree","Villes","0000000000","melvyn2701@gmail.com","etudiant", client, 1);
+    affiche_all(client, 0);
+    remplirClient("Liam", "Luka", "Lyon", "0000000000", "liamlucas@wanadoo.fr", "éleveur de pommes de terre", client, 2);
+    affiche_all(client, 1);
+    modifieClient("Lucas", 1, client, 2);
+    affiche_all(client, 1);
+    printf("%d", recherche(client, "Melvyn"));
+    affiche_all(client, 0);
+    return 0;
 }
