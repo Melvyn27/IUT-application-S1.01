29a9b6800ca71b2612abd618b5a5a618e866b4cc
Melvyn27 <melvyn2701@gmail.com>
2022-01-09 11:37:36 +0100

    fix: module

--- afficheListe.c
+++ afficheListe.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "header.h"
 
 void affiche(struct abonne client[]){
--- cmake-build-debug/affiche.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by Melvyn Delpree on 14/12/2021.
-//
-
-int affiche (){
-printf(test.nom);
-printf(test.prenom);
-return 1;
-}
